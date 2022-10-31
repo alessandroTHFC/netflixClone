bdf02bb1a06314c2ba2ebd1c21b9bb97173563a6
alessandroTHFC <apangraz@SRA-LT-2033.sra.com.au>
2022-10-31 15:04:18 +1030

    inc src files for UI

---
+++ netClone/netflix-build/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDZXIHjaIertZsliBPgiDGWZ-t1XY71tK8",
+  authDomain: "netflix-clone-3f482.firebaseapp.com",
+  projectId: "netflix-clone-3f482",
+  storageBucket: "netflix-clone-3f482.appspot.com",
+  messagingSenderId: "805615930634",
+  appId: "1:805615930634:web:5e13e8fec65c628f6cbef3",
+};
